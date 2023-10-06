e53e74a27c1fe3a58639d8c035d090e885830fec
Matheus Cassemiro <127998481+matheuscassemiro1@users.noreply.github.com>
2023-10-06 09:52:05 -0300

    Update app.module.ts

--- multimarcas-frontend/src/app/app.module.ts
+++ multimarcas-frontend/src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
+import { LoginComponent } from './login/login.component';
+import { Router, RouterModule } from '@angular/router';
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: ListaProdutosComponent },
+      { path: 'login', component: LoginComponent }])
+  ],
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListaProdutosComponent
+    ListaProdutosComponent,
+    LoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
