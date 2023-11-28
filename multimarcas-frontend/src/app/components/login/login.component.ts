17126d84678e2eb9fcec93a4cd9e43869573f7f6
matheuscassemiro1 <127998481+matheuscassemiro1@users.noreply.github.com>
2023-11-28 16:25:13 -0300

    feat: add post login

--- multimarcas-frontend/src/app/components/login/login.component.ts
+++ multimarcas-frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common'
+import { Validators } from '@angular/forms';
+import { LoginService } from 'src/app/services/login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
+  constructor(
+    @Inject(DOCUMENT) document: Document,
+    private loginService: LoginService
+    ) {
+  }
+
+  formularioLogin = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    senha: new FormControl('', Validators.required),
+  })
+
+  efetuarLogin() {
+    console.log(this.formularioLogin);
+    this.loginService.tentarLogin(this.formularioLogin.value).subscribe((resultado) => (console.log(resultado)))
+  }
+
 }
