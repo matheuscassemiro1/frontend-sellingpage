34faf5b51fe6b5d2a391a4a72a4249635f4b8e2f
matheuscassemiro1 <127998481+matheuscassemiro1@users.noreply.github.com>
2024-09-27 09:17:20 -0300

    first

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/sellingpage-frontend/dist/sellingpage-frontend'));
+app.use(express.static(__dirname + '/multimarcas/dist/multimarcas'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/sellingpage-frontend/dist/sellingpage-frontend/index.html')
+    res.sendFile(__dirname + '/multimarcas/dist/multimarcas/index.html')
 });
 
 
