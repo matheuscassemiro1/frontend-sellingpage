a9c709c8abc7865eed16cbefcd3e51537d923c27
matheuscassemiro1 <127998481+matheuscassemiro1@users.noreply.github.com>
2024-09-27 09:24:06 -0300

    fix

--- sellingpage-frontend/README.md
+++ sellingpage-frontend/README.md
@@ -1,4 +1,4 @@
-# MultimarcasFrontend
+# frontend-sellingpageFrontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.4.
 
--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/multimarcas/dist/multimarcas'));
+app.use(express.static(__dirname + '/frontend-sellingpage/dist/frontend-sellingpage'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/multimarcas/dist/multimarcas/index.html')
+    res.sendFile(__dirname + '/frontend-sellingpage/dist/frontend-sellingpage/index.html')
 });
 
 
